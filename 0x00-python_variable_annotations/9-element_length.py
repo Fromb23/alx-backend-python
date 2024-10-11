7c3a539b7c15af381723bba00201ab2727e9b9ae
Francis Rombo <fromb@kali.>
2024-10-11 18:42:27 +0000

    annotations

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+     duck type an iterable object
+"""
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    return  duck type an iterable object
+    """
+    return [(i, len(i)) for i in lst]
